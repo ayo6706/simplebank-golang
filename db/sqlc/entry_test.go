9e2e73b5fcd582aa92386f1b5f2fbc8fc6dd21d1
ayomide <ayo6706@gmail.com>
2023-12-23 10:33:59 +0100

    implement test to get entry

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -3,12 +3,14 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/ayo6706/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateEntry(t *testing.T) {
+func CreateRandomEntry(t *testing.T) Entry {
+
 	account := CreateRandomAccount(t)
 
 	arg := CreateEntryParams{
@@ -24,5 +26,24 @@ func TestCreateEntry(t *testing.T) {
 	require.Equal(t, arg.Amount, entry.Amount)
 
 	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	CreateRandomEntry(t)
+
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
 
 }
