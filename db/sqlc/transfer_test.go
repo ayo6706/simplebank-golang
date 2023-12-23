ab9b0d7091f186116bab8bc987781b271b694e62
ayomide <ayo6706@gmail.com>
2023-12-23 11:22:34 +0100

    implement test to get transfer

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -34,3 +34,17 @@ func TestCreateTransfer(t *testing.T) {
 
 	CreateRandomTransfer(t, account1, account2)
 }
+
+func TestGetTransfer(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+	account2 := CreateRandomAccount(t)
+	transfer1 := CreateRandomTransfer(t, account1, account2)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+}
