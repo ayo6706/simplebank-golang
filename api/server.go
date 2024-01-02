117d4bcecc45e0b93673c231a78d47fea0b4df4f
ayomide <ayo6706@gmail.com>
2024-01-02 13:05:26 +0100

    implement create account route

--- api/server.go
+++ api/server.go
@@ -22,6 +22,10 @@ func NewServer(store *db.Store) *Server {
 	return server
 }
 
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
