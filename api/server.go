6f85526f182acef75ef4f63bc5790c638bdcc8cd
ayomide <ayo6706@gmail.com>
2024-01-02 12:13:10 +0100

    implement create account handler

--- api/server.go
+++ api/server.go
@@ -16,6 +16,12 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	router.POST("/accounts", server.createAccount)
+
 	server.router = router
 	return server
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
