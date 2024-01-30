f2bd66601e3554912e675d56e47cfe0f4bcd4103
ayomide <ayo6706@gmail.com>
2024-01-30 15:34:39 +0100

    implement verify token

--- token/payload.go
+++ token/payload.go
@@ -4,10 +4,14 @@ import (
 	"errors"
 	"time"
 
+	jwt "github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
-var ErrExpiredToken = errors.New("token has expired")
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
 
 // Payload is a payload of the token
 type Payload struct {
@@ -15,6 +19,36 @@ type Payload struct {
 	Username  string    `json:"username"`
 	IssuedAt  time.Time `json:"issued_at"`
 	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// GetAudience implements jwt.Claims.
+func (*Payload) GetAudience() (jwt.ClaimStrings, error) {
+	panic("unimplemented")
+}
+
+// GetExpirationTime implements jwt.Claims.
+func (*Payload) GetExpirationTime() (*jwt.NumericDate, error) {
+	panic("unimplemented")
+}
+
+// GetIssuedAt implements jwt.Claims.
+func (*Payload) GetIssuedAt() (*jwt.NumericDate, error) {
+	panic("unimplemented")
+}
+
+// GetIssuer implements jwt.Claims.
+func (*Payload) GetIssuer() (string, error) {
+	panic("unimplemented")
+}
+
+// GetNotBefore implements jwt.Claims.
+func (*Payload) GetNotBefore() (*jwt.NumericDate, error) {
+	panic("unimplemented")
+}
+
+// GetSubject implements jwt.Claims.
+func (*Payload) GetSubject() (string, error) {
+	panic("unimplemented")
 }
 
 func NewPayload(username string, duration time.Duration) (*Payload, error) {
